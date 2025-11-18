aa4f346354e30be9a6d24f71dc35bf2b235c3d44
BaeGyee9 <zawtunwaiz@gmail.com>
2025-11-19 02:19:51 +0700

    Update web.py

--- templates/web.py
+++ templates/web.py
@@ -612,4 +612,6 @@ def update_user():
     return jsonify({"ok": False, "err": "Invalid data"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    web_port = int(os.environ.get("WEB_PORT", "19432"))  # ✅ ပြောင်းပါ
+    app.run(host="0.0.0.0", port=web_port)
+    
